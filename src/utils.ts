a5c6700001b399bc09a0c39bce5440390710b067
Luca Pulina <luca.pulina@gmail.com>
2023-10-31 12:52:08 +0100

    feat: add utils compareDates, isLeapYear

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import { formatDate, normalizeDate } from './formatDate'
-import { Period, AddPeriodToParam } from './types'
+import { Period, AddPeriodToParam, DateLike } from './types'
 
 const periodMs: Record<Exclude<Period, 'months' | 'years'>, number> = {
     seconds: 1000,
@@ -45,4 +45,18 @@ export const subPeriodTo = ({
     operation = 'add'
 }: AddPeriodToParam) => {
     return addPeriodTo({ date, n, period, formatStr, operation: 'sub' })
+}
+
+export const isLeapYear = (year: number | string) => {
+    const numericYear: number = typeof year === 'string' ? parseInt(year, 10) : year
+    return (numericYear % 4 === 0 && numericYear % 100 !== 0) || numericYear % 400 === 0
+}
+
+export const compareDates = (date1: DateLike, date2: DateLike) => {
+    const d1 = normalizeDate(date1)
+    const d2 = normalizeDate(date2)
+
+    if (d1.getTime() < d2.getTime()) return -1
+    if (d1.getTime() === d2.getTime()) return 0
+    return 1
 }
